c60be3688dffdb1ddfe1d87c4d6909355c9a0ce1
zyj <1876849563@qq.com>
2024-11-08 09:56:52 +0800

    feat:热门推荐数据获取

--- src/pages/index/components/HotPanel.vue
+++ src/pages/index/components/HotPanel.vue
@@ -14,7 +14,7 @@ defineProps<{
                 <text class="title-text">{{ item.title }}</text>
                 <text class="title-desc">{{ item.alt }}</text>
             </view>
-            <navigator hover-class="none" url="/pages/hot/hot" class="cards">
+            <navigator hover-class="none" :url="`/pages/hot/hot?type=${item.type}`" class="cards">
                 <image class="image" mode="aspectFit" :src="src" v-for="src in item.pictures" :key="src"></image>
             </navigator>
         </view>
