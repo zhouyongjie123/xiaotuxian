2576d2df4789e4cd844aec47fba6a4e8d83513f7
zyj <1876849563@qq.com>
2024-11-15 19:40:58 +0800

    feat:商品分类-二级分类渲染和商品渲染

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { getHomeBannerAPI } from '@/services/home'
 import type { BannerItem } from '@/types/home'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { getCategoryTopAPI } from '@/services/category'
 import type { CategoryTopItem } from '@/types/category'
@@ -25,6 +25,11 @@ const getCategoryTopData = async () => {
 onLoad(() => {
   getBannerData()
   getCategoryTopData()
+})
+
+// 提取当前二级分类数据
+const subCategoryList = computed(() => {
+  return categoryList.value[activeIndex.value]?.children || []
 })
 </script>
 
@@ -55,27 +60,27 @@ onLoad(() => {
         <!-- 焦点图 -->
         <XtxSwiper class="banner" :list="bannerList" />
         <!-- 内容区域 -->
-        <view class="panel" v-for="item in 3" :key="item">
+        <view class="panel" v-for="item in subCategoryList" :key="item.id">
           <view class="title">
-            <text class="name">宠物用品</text>
+            <text class="name">{{ item.name }}</text>
             <navigator class="more" hover-class="none">全部</navigator>
           </view>
           <view class="section">
             <navigator
-              v-for="goods in 4"
-              :key="goods"
+              v-for="goods in item.goods"
+              :key="goods.id"
               class="goods"
               hover-class="none"
-              :url="`/pages/goods/goods?id=`"
+              :url="`/pages/goods/goods?id=${goods.id}`"
             >
               <image
                 class="image"
-                src="https://yanxuan-item.nosdn.127.net/674ec7a88de58a026304983dd049ea69.jpg"
+                :src="goods.picture"
               ></image>
-              <view class="name ellipsis">木天蓼逗猫棍</view>
+              <view class="name ellipsis">{{ goods.name }}</view>
               <view class="price">
                 <text class="symbol">¥</text>
-                <text class="number">16.00</text>
+                <text class="number">{{ goods.price }}</text>
               </view>
             </navigator>
           </view>
